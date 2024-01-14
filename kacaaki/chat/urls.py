0a0ea732115dcdbb52eb20ef6a7a13b07c6d0698
Saurav Aryal <sauravaryal@Sauravs-MacBook-Pro.local>
2024-01-14 13:02:55 +0545

    chat app

--- kacaaki/chat/urls.py
+++ kacaaki/chat/urls.py
@@ -9,4 +9,5 @@ app_name = 'chat'
 urlpatterns = [
     re_path(r'^room/list/$',ChatRoomListView.as_view(),name='chatroom-list'),
     re_path(r'^user/(?P<pk>\d+)/$', ChatRoomView.as_view(), name='chatting-room'),
+    re_path(r'^load-more-messages/$', load_more_messages, name='load-more-messages'),
 ]
--- kacaaki/chat/views.py
+++ kacaaki/chat/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from django.views import View
-from .models import ChatRoom,ChatMessage
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 from users.models import User
+from .models import ChatRoom,ChatMessage
+from django.core import serializers
 
 
 # Create your views here.
 
 
 class ChatRoomListView(View):
+    
+
     template_name = 'chat/chatroom.html'
     def get(self,request):
         chat_rooms = ChatRoom.objects.all()
@@ -23,9 +28,23 @@ class ChatRoomView(View):
     
     def get(self,request,pk):
         chat_room = ChatRoom.objects.get(id=pk)
-        chat_messages = ChatMessage.objects.filter(room=chat_room).order_by('timestamp')
+        chat_messages = ChatMessage.objects.filter(room=chat_room).order_by('-timestamp')[:10]
         context = {
             'chat_room':chat_room,
-            'chat_messages':chat_messages,
+            'chat_messages':reversed(chat_messages),
         }
         return render(request,self.template_name,context)
+    
+    
+
+def load_more_messages(request):
+    room_id = request.GET.get('room_id')
+    offset = int(request.GET.get('offset'))
+
+    room = ChatRoom.objects.get(id=room_id)
+    messages = ChatMessage.objects.filter(room=room).order_by('-timestamp')[offset:offset + 10]
+
+    serialized_messages = serializers.serialize('json', messages)
+    
+
+    return JsonResponse({'messages':serialized_messages}, safe=False)
